3320df8b0d2b7fd8640b01cf9362e4419a5419a6
ShyamSundaraChary <shyamsundarachary22@gmail.com>
2025-07-18 15:44:01 +0530

    implemented the type writer design to make ui look attractive

--- components/typewriter-effect.jsx
+++ components/typewriter-effect.jsx
@@ -8,6 +8,33 @@ export function TypewriterEffect({ words, className = "" }) {
   const [currentText, setCurrentText] = useState("");
   const [isDeleting, setIsDeleting] = useState(false);
 
+  useEffect(() => {
+    const typeSpeed = isDeleting ? 50 : 100;
+    const word = words[currentWordIndex];
+
+    const timeout = setTimeout(() => {
+      if (!isDeleting) {
+        // Typing
+        if (currentText.length < word.length) {
+          setCurrentText(word.substring(0, currentText.length + 1));
+        } else {
+          // Finished typing, wait then start deleting
+          setTimeout(() => setIsDeleting(true), 1500);
+        }
+      } else {
+        // Deleting
+        if (currentText.length > 0) {
+          setCurrentText(currentText.substring(0, currentText.length - 1));
+        } else {
+          // Finished deleting, move to next word
+          setIsDeleting(false);
+          setCurrentWordIndex((prev) => (prev + 1) % words.length);
+        }
+      }
+    }, typeSpeed);
+
+    return () => clearTimeout(timeout);
+  }, [currentText, isDeleting, currentWordIndex, words]);
 
   return (
     <div className={`inline-flex items-center ${className}`}>
